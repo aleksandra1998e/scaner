3a8cd17ae52a6f3e3b653a2e4d7d5743987b6dd2
Александра Епифанова <aleksandra.epifanova.work@gmail.com>
2022-06-05 20:51:58 +0300

    add try-except

--- scan/app.py
+++ scan/app.py
@@ -14,23 +14,29 @@ logger.addHandler(handler)
 
 
 async def counter(request):
-    logger.info(f"recd ip: {request.match_info['ip']}; begin port: {request.match_info['begin_port']}; "
-                f"end port: {request.match_info['end_port']}")
-    begin_port = int(request.match_info['begin_port'])
-    end_port = int(request.match_info['end_port'])
-    tasks = [scan4port(request.match_info['ip'], i) for i in range(begin_port, end_port + 1)]
-    logger.info(f"Search start")
-    res = await asyncio.gather(*tasks)
-    logger.info("Result processing")
-    response = []
-    for i, j in res:
-        if i == 'None':
-            response.append({"port": j, "state": 'close'})
-        else:
-            response.append({"port": j, "state": 'open'})
-    res = json.dumps(response)
-    logger.info("Results are ready")
-    return web.json_response(res)
+    try:
+        logger.info(f"recd ip: {request.match_info['ip']}; begin port: {request.match_info['begin_port']}; "
+                    f"end port: {request.match_info['end_port']}")
+        begin_port = int(request.match_info['begin_port'])
+        end_port = int(request.match_info['end_port'])
+        if end_port < begin_port:
+            begin_port, end_port = end_port, begin_port
+        tasks = [scan4port(request.match_info['ip'], i) for i in range(begin_port, end_port + 1)]
+        logger.info(f"Search start")
+        res = await asyncio.gather(*tasks)
+        logger.info("Result processing")
+        response = []
+        for i, j in res:
+            if i == 'None':
+                response.append({"port": j, "state": 'close'})
+            else:
+                response.append({"port": j, "state": 'open'})
+        res = json.dumps(response)
+        logger.info("Results are ready")
+        return web.json_response(res)
+    except ValueError:
+        logger.exception('Ports are not numbers')
+        return web.Response(text="Ports must be numbers")
 
 
 async def scan4port(ip, port):
